78a0c09b2599c7d4d299009027df4e250453a954
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2024-06-30 15:21:18 +0530

    first init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+    import Vegam from "vegamjs";
+
+    
+    const s=new Vegam()
+
+    s.get("/",ctx=>{
+        ctx.res.json({message:"hai"})
+    })
+
+    s.listen(8000,()=>{
+        console.log("server listen to port 8000")
+    })
